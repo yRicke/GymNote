1f3f001970ab25bc82fa3605c40d743b62cb35c9
Henrique Vieira <henriquevieiraperes@gmail.com>
2026-08-16 20:01:49 -0300

    chore: preparar projeto para deploy na Vercel

--- setup/settings.py
+++ setup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ql(69wffjr!n$e9i$%hw0-7nw+lzipwt2fg0-z8hn2*pvo5kiq'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-ql(69wffjr!n$e9i$%hw0-7nw+lzipwt2fg0-z8hn2*pvo5kiq',
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('VERCEL_ENV') is None
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
